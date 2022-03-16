1298a0795444c041644905b7bbafb684a3111a57
Munji Kahalah <kahalahmunji@gmail.com>
2022-03-16 11:37:33 -0600

    finishing up our models now we will work on our api

--- api/fast.py
+++ api/fast.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import requests
 
 app = FastAPI()
+base_url = 'https://financialmodelingprep.com'
+api_key = 'apikey=3845af5c988287d80bbe6a9e83d56c30'
 
 app.add_middleware(
     CORSMiddleware,
@@ -16,6 +19,12 @@ app.add_middleware(
 def api_root():
     return {"Please provide a query"}
 
-@app.get("/hello")
-def hello():
-    return {"Hello": "World"}
+@app.get("/predict")
+def predict():
+    # balance sheet statement
+    stock = 'NVDA'
+    params = f'/api/v3/balance-sheet-statement/{stock}?limit=120'
+    response = requests.get(base_url + params + api_key).json()
+    print(response)
+    # return {response}
+    # return response[0]
