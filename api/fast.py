76bc80ee7cf396244e9e85a876a30e4dc4f4800b
Munji Kahalah <kahalahmunji@gmail.com>
2022-03-23 13:40:06 -0600

    basic API functions and calling column names

--- api/fast.py
+++ api/fast.py
@@ -1,11 +1,10 @@
 from webbrowser import get
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from api import stock
 import requests
-from api import test
 import time
 import http3
+import pandas as pd
 
 app = FastAPI()
 client = http3.AsyncClient()
@@ -22,7 +21,6 @@ app.add_middleware(
 
 
 async def call_api(url: str):
-
     r = await client.get(url)
     return r.json()
 
@@ -35,9 +33,35 @@ def api_root():
 async def predict(symbol):
     # balance sheet statement
     params = f'/api/v3/balance-sheet-statement/{symbol}?limit=120&'
-    #time.sleep(10)
     try:
         res1 = await call_api(base_url + params + api_key)
-        return {"res": res1}
+        # return {"res": res1}
     except:
         return {"error"}
+    """ Procedure:
+        1.) Grab our values from the .JSON store it in X variable
+        2.) Create another variable with the list of column names
+        3.) Zip these two to form a Dict, then assign the dict as a DataFrame -> dict(zip(a, b))
+        4.) Feed the joblib file with the newly dataframe
+        5.) Create a prediction
+        """
+    # Grab our values from the .JSON store it in X variable
+    X = {res1[0]['calendarYear'],
+         res1[0]['longTermInvestments']
+         }
+    # Create another variable with the list of column names
+    colimn_names = ["calendarYear",
+                "GPM",
+                "A (SGA)",
+                "B (RD)",
+                "C (PPE)",
+                "D (DEPR)",
+                "E (CAPEX)",
+                "F (NR)",
+                "currentRatio",
+                "G (ROA)",
+                "H (LD/GP)",
+                "debtToEquity",
+                "Net Issuance",
+                "Interest - income"]
+    return colimn_names
