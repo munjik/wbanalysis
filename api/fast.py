1f055133a27da532f49e5e3b673944f2248faad4
Munji Kahalah <kahalahmunji@gmail.com>
2022-03-28 12:11:32 -0600

    fixing api from financial dashboard to YFinance with Leo

--- api/fast.py
+++ api/fast.py
@@ -5,11 +5,15 @@ import requests
 import time
 import http3
 import pandas as pd
+import yahoo_fin.stock_info as yf
 
 app = FastAPI()
 client = http3.AsyncClient()
-base_url = 'https://financialmodelingprep.com'
-api_key = 'apikey=3845af5c988287d80bbe6a9e83d56c30'
+base_url = 'https://yfapi.net/v11/finance/quoteSummary/'
+# api_key = 'apikey=CmVlEza5Un9eIBTyvq7zea5Yk6wcPszN9UEYsUvF'
+headers = {
+    'x-api-key': "CmVlEza5Un9eIBTyvq7zea5Yk6wcPszN9UEYsUvF"
+    }
 
 app.add_middleware(
     CORSMiddleware,
@@ -20,7 +24,8 @@ app.add_middleware(
 )
 
 async def call_api(url: str):
-    r = await client.get(url)
+
+    r = await client.get(url,headers=headers)
     return r.json()
 
 # define a root '/' endpoint
@@ -31,22 +36,16 @@ def api_root():
 @app.get("/predict/{symbol}")
 async def predict(symbol):
     # balance sheet statement
-    params_bs = f'/api/v3/balance-sheet-statement/{symbol}?limit=120&'
+    params_bs = f'{symbol}?lang=en&region=US&modules=balanceSheetHistory'
     # income sheet statement
-    params_income = f'/api/v3/income-statement/{symbol}?limit=120&'
+    params_income = f'{symbol}?lang=en&region=US&modules=incomeStatementHistory'
     # cash flow statement
-    params_cash = f'/api/v3/cash-flow-statement/{symbol}?limit=120&'
-
-    # try:
-    #     balance_sheet = await call_api(base_url + params_bs + api_key)
-    #     # return {"res": res1}
-    # except:
-    #     return {"error"}
+    params_cash = f'{symbol}?lang=en&region=US&modules=cashflowStatementHistory'
 
     # grab our 3 API's with business info needed for model
-    balance_sheet = await call_api(base_url + params_bs + api_key)
-    income_statement = await call_api(base_url + params_income + api_key )
-    cashflow_statement = await call_api(base_url + params_cash + api_key )
+    balance_sheet = await call_api(base_url + params_bs)
+    income_statement = await call_api(base_url + params_income)
+    cashflow_statement = await call_api(base_url + params_cash)
 
     """ Procedure:
         ***KEEP IN MIND:FINANCIAL MODELING GROUP IS FOR HISTORICAL DATA TO TRAIN OUR MODEL WITH.
@@ -60,49 +59,11 @@ async def predict(symbol):
         """
 
     # BALANCE SHEET API
-    NR = balance_sheet[0]['retainedEarnings']
-    CA = balance_sheet[0]["totalCurrentAssets"]
-    CL = balance_sheet[0]["totalCurrentLiabilities"]
-    TA = balance_sheet[0]["totalAssets"]
-    SE = balance_sheet[0]["totalStockholdersEquity"]
-    PPE =balance_sheet[0]["propertyPlantEquipmentNet"]
-    SD = balance_sheet[0]["shortTermDebt"]
-    LD = balance_sheet[0]["longTermDebt"]
-    # TS = balance_sheet[0]["treasuryStock"]
-    # CS = balance_sheet[0]["cash"]
-    # X = {balance_sheet[0]['calendarYear'],
-    #      balance_sheet[0]['longTermInvestments']
-    #      }
+
 
     # INCOME STATEMENT API
-    NI = income_statement[0]["netIncome"]
-    GP = income_statement[0]["grossProfit"]
-    SGA = income_statement[0]["sellingGeneralAndAdministrativeExpenses"]
-    RD = income_statement[0]["researchAndDevelopmentExpenses"]
-    TR = income_statement[0]["revenue"]
-    IE = income_statement[0]["interestExpense"]
-    OE = income_statement[0]["operatingExpenses"]
-    OI = GP - OE # Operating Income = Gross Profit - Operating Expense
 
     # CASHFLOW STATEMENT API
-    CE = abs(cashflow_statement[0]["capitalExpenditures"])
-    DE = cashflow_statement[0]["depreciation"]
-    IS = cashflow_statement[0]["issuanceOfStock"] # (+)
-    RS = cashflow_statement[0]["repurchaseOfStock"] # (-)
 
-    # Create another variable with the list of column names
-    colimn_names = ["calendarYear",
-                "GPM",
-                "A (SGA)",
-                "B (RD)",
-                "C (PPE)",
-                "D (DEPR)",
-                "E (CAPEX)",
-                "F (NR)",
-                "currentRatio",
-                "G (ROA)",
-                "H (LD/GP)",
-                "debtToEquity",
-                "Net Issuance",
-                "Interest - income"]
+
     return cashflow_statement
